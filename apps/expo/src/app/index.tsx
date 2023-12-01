af30c6ff0ba8f7b49b0fd3d493b1658b87c525bc
Matt Gramigna <matthewgramigna@gmail.com>
2023-12-01 01:09:39 -0500

    fix: remove old router so typecheck passes

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -2,18 +2,13 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Stack } from 'expo-router';
-import { api } from '@/utils/api';
 
 const Index = () => {
-  const message = api.example.hello.useQuery({
-    message: 'world!',
-  });
-
   return (
     <SafeAreaView className="">
       <Stack.Screen options={{ title: 'Home Page' }} />
       <View className="h-full w-full p-4">
-        <Text>{message.data}</Text>
+        <Text>Hello world</Text>
       </View>
     </SafeAreaView>
   );
