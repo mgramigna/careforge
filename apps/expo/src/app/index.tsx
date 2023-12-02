8b80f0d943e94d724e1b9e6309398d2ead4e2869
Matt Gramigna <matthewgramigna@gmail.com>
2023-12-01 23:38:08 -0500

    feat: proof of concept api query working in expo

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ActivityIndicator, Alert, FlatList, Pressable, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Stack } from 'expo-router';
+import { api } from '@/utils/api';
 
 const Index = () => {
+  const { data: bundle, isLoading } = api.patient.search.useQuery({
+    active: true,
+  });
+
+  if (isLoading) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+  }
+
   return (
     <SafeAreaView className="">
       <Stack.Screen options={{ title: 'Home Page' }} />
-      <View className="h-full w-full p-4">
-        <Text>Hello world</Text>
+      <View className="h-full w-full">
+        <FlatList
+          data={bundle?.entry?.map(({ resource }) => resource) ?? []}
+          keyExtractor={(patient) => patient.id}
+          renderItem={({ item: patient }) => (
+            <Pressable
+              onPress={() => {
+                Alert.alert('hey');
+              }}
+            >
+              <View className="w-full items-center border border-slate-200 p-8">
+                <Text>
+                  {patient.name.at(0)?.given?.join('-')} {patient.name.at(0)?.family}
+                </Text>
+              </View>
+            </Pressable>
+          )}
+        />
       </View>
     </SafeAreaView>
   );
