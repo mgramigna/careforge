e6387aea5517ddad23ba4d1347e61c6c8a06da60
Matt Gramigna <matthewgramigna@gmail.com>
2023-12-12 18:53:47 -0500

    feat: add nextjs serverless handler for trpc

---
+++ apps/nextjs/next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: ['@canvas-challenge/api'],
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default config;
---
+++ apps/nextjs/src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,39 @@
+import { type NextRequest } from 'next/server';
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+
+import { appRouter, createTRPCContext } from '@canvas-challenge/api';
+
+export const runtime = 'edge';
+
+function setCorsHeaders(res: Response) {
+  res.headers.set('Access-Control-Allow-Origin', '*');
+  res.headers.set('Access-Control-Request-Method', '*');
+  res.headers.set('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+  res.headers.set('Access-Control-Allow-Headers', '*');
+}
+
+export function OPTIONS() {
+  const response = new Response(null, {
+    status: 204,
+  });
+  setCorsHeaders(response);
+  return response;
+}
+
+const handler = async (req: NextRequest) => {
+  const response = await fetchRequestHandler({
+    endpoint: '/api/trpc',
+    // @ts-expect-error testing
+    router: appRouter,
+    req,
+    createContext: () => createTRPCContext(),
+    onError({ error, path }) {
+      console.error(`>>> tRPC Error on '${path}'`, error);
+    },
+  });
+
+  setCorsHeaders(response);
+  return response;
+};
+
+export { handler as GET, handler as POST };
